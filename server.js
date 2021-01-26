48f04c744609f3b5972501634ea9801a8a4f072d
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-01-25 20:04:00 -0800

    spaces to tabs

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import NodeMediaServer from 'node-media-server';
 
 const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*'
-  }
+	rtmp: {
+		port: 1935,
+		chunk_size: 60000,
+		gop_cache: true,
+		ping: 30,
+		ping_timeout: 60
+	},
+	http: {
+		port: 8000,
+		allow_origin: '*'
+	}
 };
 
 const nms = new NodeMediaServer(config);
